7ea2a9b8eb12cbbff47109294faa2dad60b48824
luthfan.maftuh <luthfanmaftuh@gmail.com>
2020-01-23 22:51:38 +0700

    Styling & Theming the App

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shop App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductOverviewScreen(),
     );
